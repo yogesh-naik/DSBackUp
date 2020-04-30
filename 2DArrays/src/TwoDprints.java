6c3054908c1eeb64250fbda0f5b3b22dc105db5b
yogesh-naik <nike.yogesh@gmail.com>
2020-04-29 19:32:27 -0700

    Update TwoDprints.java

--- 2DArrays/src/TwoDprints.java
+++ 2DArrays/src/TwoDprints.java
@@ -1,40 +1,70 @@
 
 public class TwoDprints {
-
+/* All patterns  given on this link 
+	https://www.phptpoint.com/print-star-pattern-in-java/
+*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-//		*
-//		**
-//		***
-//		****
-//		*****
 
-//		for (int i = 1; i <= 5; i++) {
-//			for (int j = 1; j <= i; j++) {
-//				System.out.print("*");
-//			}
-//			System.out.println("");
-//		}
-		
-		for (int i = 5; i>=1; i--) {
+		// *
+		// **
+		// ***
+		// ****
+		// *****
+
+		// for (int i = 1; i <= 5; i++) {
+		// for (int j = 1; j <= i; j++) {
+		// System.out.print("*");
+		// }
+		// System.out.println("");
+		// }
+
+		for (int i = 5; i >= 1; i--) {
 			for (int j = i; j >= 1; j--) {
 				System.out.print("*");
-			}			
+			}
 			System.out.println("");
-			for (int j = 5-i; j >= 0; j--){
+			for (int j = 5 - i; j >= 0; j--) {
 				System.out.print(" ");
 			}
 
 		}
-		
-		String str = "naik";
-		StringBuffer sb =  new StringBuffer("Yogesh");
-		for(int i=0;i<=str.length()-1;i++)
-				sb.append(str.charAt(i));
-		
-		System.out.println(sb);
-		
-	}
+		System.out.println();
+		System.out.println("////////////////////////////////////////////////////");
+		for (int i = 0; i <= 5; i++) {
+			for (int k = 5; k > i; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
 
+			System.out.println();
+		}
+
+		for (int i = 4; i >= 1; i--) {
+			for (int k = 4; k >= i; k--) {
+				System.out.print(" ");
+			}
+			//
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println("////////////////////////////////////////////////////");
+		for (int i = 0; i <= 4; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		for (int i = 4; i >= 0; i--) {
+			for (int j = i; j >= 0; j--) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	System.out.println("////////////////////////////////////////////////////");
+	}
 }
